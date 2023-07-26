788ccf6f7c09184cd85f2a3b26c50334220f973e
Vivekkumar0412 <www.vivekkumar0412@gmail.com>
2023-07-26 22:56:13 +0530

    Program to reverse an array

--- index.cpp
+++ index.cpp
@@ -1,6 +1,21 @@
-#include<bits/stdc++.h>
+// Online C++ compiler to run C++ program online
+#include <iostream>
 using namespace std;
-int main(){
-    cout<<"Namaste Arrays";
+int main() {
+    int arr[] = {1,2,3,4,5,6};
+    int i = 0;
+    int j = 5;
+    while(i<j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    };
+    int k = 0;
+    while( k<=5){
+        cout<<arr[k]<<" ";
+        k++;
+    }
     return 0;
 }
