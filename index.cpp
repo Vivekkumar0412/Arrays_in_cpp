1bbffd8eaf1aec5b51b93125a692496eb02fe455
Vivekkumar0412 <www.vivekkumar0412@gmail.com>
2023-07-28 15:52:57 +0530

    swap alternates in an array

--- index.cpp
+++ index.cpp
@@ -22,36 +22,52 @@
 // };
 
 // Find triplets in the array
+// #include <iostream>
+// using namespace std;
+// int main() {
+// int arr[] ={1, 2, 3, 1, 2, 3};
+// int n = 6;
+// int K = 4;
+// for(int i = 0; i<n; i++)
+// {
+// 		for(int j = i +1; j<n; j++)
+//         {
+// 			for(int m = j+1; m<n; m++)
+//             {
+// 				if(arr[i] + arr[j] + arr[m] == K)
+//                 {
+// 					cout<<arr[i]<<" "<<arr[j]<<" "<<arr[m]<<endl;
+// 				}
+// 			}
+// 		}
+// 	};
+// };
+
+// SWAP ALTERNATES
 #include <iostream>
 using namespace std;
-int main() {
-int arr[] ={1, 2, 3, 1, 2, 3};
-int n = 6;
-int K = 4; 
-for(int i = 0; i<n; i++)
+void printArr(int arr[], int n)
 {
-		for(int j = i +1; j<n; j++)
-        {
-			for(int m = j+1; m<n; m++)
-            {
-				if(arr[i] + arr[j] + arr[m] == K)
-                {
-					cout<<arr[i]<<" "<<arr[j]<<" "<<arr[m]<<endl;
-				}
-			}
-		}
-	};
-};
-// 4
-// 5
-// 10 5 5 5 2
-// 12
-// 6
-// 1 2 3 1 2 3
-// 6
-// 4
-// 1 2 3 4 
-// 11
-// 6
-// 1 1 2 2 1 1
-// 4
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    };
+}
+void swapArr(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
+        i++;
+    };
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 9,11};
+    int n = 9;
+    swapArr(arr,n);
+    printArr(arr, n);
+    return 0;
+}
