ab5766fa749613f0602e950cc532727070f5ad3a
Vivekkumar0412 <www.vivekkumar0412@gmail.com>
2023-08-14 20:18:29 +0530

    reverse an array

--- index.cpp
+++ index.cpp
@@ -1,25 +1,25 @@
 // Online C++ compiler to run C++ program online
 // REVERSE AN ARRAY
-// #include <iostream>
-// using namespace std;
-// int main() {
-//     int arr[] = {1,2,3,4,5,6};
-//     int i = 0;
-//     int j = 5;
-//     while(i<j){
-//         int temp = arr[i];
-//         arr[i] = arr[j];
-//         arr[j] = temp;
-//         i++;
-//         j--;
-//     };
-//     int k = 0;
-//     while( k<=5){
-//         cout<<arr[k]<<" ";
-//         k++;
-//     }
-//     return 0;
-// };
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[] = {1,2,3,4,5,6};
+    int i = 0;
+    int j = 5;
+    while(i<j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    };
+    int k = 0;
+    while( k<=5){
+        cout<<arr[k]<<" ";
+        k++;
+    }
+    return 0;
+};
 
 // Find triplets in the array
 // #include <iostream>
